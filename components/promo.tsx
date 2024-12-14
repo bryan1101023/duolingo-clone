b0d704a20fcc3a0f0ac558452ac51bdfebd87175
bryan <alexandrosavgoustiss@gmail.com>
2024-12-14 13:25:49 +0200

    Update promo.tsx

--- components/promo.tsx
+++ components/promo.tsx
@@ -10,14 +10,14 @@ export const Promo = () => {
         <div className="flex items-center gap-x-2">
           <Image src="/unlimited.svg" alt="Pro" height={26} width={26} />
 
-          <h3 className="text-lg font-bold">Upgrade to Pro</h3>
+          <h3 className="text-lg font-bold">Αναβάθμισε σε PROlingo!</h3>
         </div>
 
-        <p className="text-muted-foreground">Get unlimited hearts and more!</p>
+        <p className="text-muted-foreground">Πάρε απεριόριστα lingohearts!</p>
       </div>
 
       <Button variant="super" className="w-full" size="lg" asChild>
-        <Link href="/shop">Upgrade today</Link>
+        <Link href="/shop">Κατάστημα</Link>
       </Button>
     </div>
   );
