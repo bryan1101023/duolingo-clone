8068a225b1fc488bd073892812b0541da81317d1
bryan <alexandrosavgoustiss@gmail.com>
2024-12-14 16:17:55 +0200

    Update header.tsx

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,10 +1,10 @@
 import {
   ClerkLoaded,
   ClerkLoading,
+  SignInButton,
   SignedIn,
   SignedOut,
   UserButton,
-  Waitlist,
 } from "@clerk/nextjs";
 import { Loader } from "lucide-react";
 import Image from "next/image";
@@ -18,6 +18,7 @@ export const Header = () => {
       <div className="mx-auto flex h-full items-center justify-between lg:max-w-screen-lg">
         <Link href="/" className="flex items-center gap-x-3 pb-7 pl-4 pt-8">
           <Image src="/mascot.svg" alt="Mascot" height={40} width={40} />
+
           <h1 className="text-2xl font-extrabold tracking-wide text-green-600">
             Lingo
           </h1>
@@ -27,18 +28,21 @@ export const Header = () => {
           <ClerkLoading>
             <Loader className="h-5 w-5 animate-spin text-muted-foreground" />
           </ClerkLoading>
-
           <ClerkLoaded>
             <SignedIn>
               <UserButton afterSignOutUrl="/" />
             </SignedIn>
 
             <SignedOut>
-              <Waitlist>
+              <SignInButton
+                mode="modal"
+                afterSignInUrl="/learn"
+                afterSignUpUrl="/learn"
+              >
                 <Button size="lg" variant="ghost">
-                  Μπες στην λίστα!
+                  Login
                 </Button>
-              </Waitlist>
+              </SignInButton>
             </SignedOut>
           </ClerkLoaded>
         </div>
